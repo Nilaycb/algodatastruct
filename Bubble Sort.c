921c0c0d8fa0f843f4c4b4abe417ea7dade82ec2
Nilay Chandra Barman <nilay.inbox@gmail.com>
2018-10-07 03:39:21 +0600

    Bubble Sort algorithm added.

---
+++ Bubble Sort.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+void dispArr(int arr[], int n)
+{
+    int i;
+
+    printf("The Array: ");
+    for(i=0; i<n; i++)
+    {
+        printf("%d", arr[i]);
+        (i != n-1) ? printf(" ") : printf("\n");
+    }
+}
+
+
+void bubbleSort(int arr[], int n)
+{
+    int i, j, temp;
+    for(i=0; i<n-1; i++)
+    {
+        for(j=0; j<n-i-1; j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    int n, i, j;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array data: ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("\n");
+
+    dispArr(arr, n);
+    printf("\n");
+
+    bubbleSort(arr, n);
+    printf("After Bubble Sorting: ");
+    dispArr(arr, n);
+
+    return 0;
+}
