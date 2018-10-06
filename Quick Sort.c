6d640c232c5141d75a076bd6ec5fe036fe5938c4
Nilay Chandra Barman <nilay.inbox@gmail.com>
2018-10-07 03:41:55 +0600

    Quick Sort in C added.

---
+++ Quick Sort.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+
+void dispArr(int arr_0[], int n)
+{
+    int i;
+
+    printf("The Array: ");
+    for(i=0; i<n; i++)
+    {
+        printf("%d", arr_0[i]);
+        (i != n-1) ? printf(" ") : printf("\n");
+    }
+}
+
+
+int qPartion(int arr_0[], int l, int r)
+{
+    int p = arr_0[r];
+
+    int i=0, j=0, temp;
+
+    for(j=0; j <= r-1; j++)
+    {
+        if(arr_0[j] <= p)
+        {
+            temp = arr_0[j];
+            arr_0[j] = arr_0[i];
+            arr_0[i] = temp;
+            i++;
+        }
+    }
+
+    //now set pivot to it's correct position
+    temp = arr_0[i];
+    arr_0[i] = arr_0[r]; // pivot placed on correct position
+    arr_0[r] = temp;
+
+    return i;
+}
+
+
+void quickSort(int arr_0[], int l, int r)
+{
+    if(l < r)
+    {
+        int p = qPartion(arr_0, l, r);
+
+        quickSort(arr_0, l, p-1);
+        quickSort(arr_0, p+1, r);
+    }
+}
+
+
+int main()
+{
+    int n, i, j;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr_0[n];
+
+    printf("Enter array data: ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr_0[i]);
+    }
+    printf("\n");
+
+    dispArr(arr_0, n);
+    printf("\n");
+
+    quickSort(arr_0, 0, n-1);
+    printf("After Quick Sorting: ");
+    dispArr(arr_0, n);
+
+    return 0;
+}
