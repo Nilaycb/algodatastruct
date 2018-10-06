7e71f4042ea06ae1ccb37cdff9eedab8d347474c
Nilay Chandra Barman <nilay.inbox@gmail.com>
2018-10-07 04:47:51 +0600

    First element as pivot position function added.

--- Quick Sort.c
+++ Quick Sort.c
@@ -13,7 +13,35 @@ void dispArr(int arr_0[], int n)
 }
 
 
-int qPartion(int arr_0[], int l, int r)
+//first element as pivot
+int qPartion_f(int arr_0[], int l, int r)
+{
+    int p = arr_0[l];
+
+    int i=r, j, temp;
+
+    for(j=l+1; j <= r; j++)
+    {
+        if(arr_0[j] >= p)
+        {
+            temp = arr_0[j];
+            arr_0[j] = arr_0[i];
+            arr_0[i] = temp;
+            i--;
+        }
+    }
+
+    //now set pivot to it's correct position
+    temp = arr_0[i];
+    arr_0[i] = arr_0[l]; // pivot placed on correct position
+    arr_0[l] = temp;
+
+    return i;
+}
+
+
+//last element as pivot
+int qPartion_l(int arr_0[], int l, int r)
 {
     int p = arr_0[r];
 
@@ -43,7 +71,7 @@ void quickSort(int arr_0[], int l, int r)
 {
     if(l < r)
     {
-        int p = qPartion(arr_0, l, r);
+        int p = qPartion_l(arr_0, l, r); //change this function according to pivot position
 
         quickSort(arr_0, l, p-1);
         quickSort(arr_0, p+1, r);
