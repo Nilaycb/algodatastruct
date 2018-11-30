c6eb699025647f4283baaac8eab06b33cce61260
Nilay Chandra Barman <nilay.inbox@gmail.com>
2018-12-01 01:10:29 +0600

    first element as pivot code removed (bug)..

--- Quick Sort.c
+++ Quick Sort.c
@@ -13,33 +13,6 @@ void dispArr(int arr_0[], int n)
 }
 
 
-//first element as pivot
-int qPartion_f(int arr_0[], int l, int r)
-{
-    int p = arr_0[l];
-
-    int i=r, j, temp;
-
-    for(j=l+1; j <= r; j++)
-    {
-        if(arr_0[j] >= p)
-        {
-            temp = arr_0[j];
-            arr_0[j] = arr_0[i];
-            arr_0[i] = temp;
-            i--;
-        }
-    }
-
-    //now set pivot to it's correct position
-    temp = arr_0[i];
-    arr_0[i] = arr_0[l]; // pivot placed on correct position
-    arr_0[l] = temp;
-
-    return i;
-}
-
-
 //last element as pivot
 int qPartion_l(int arr_0[], int l, int r)
 {
@@ -71,8 +44,7 @@ void quickSort(int arr_0[], int l, int r)
 {
     if(l < r)
     {
-        int p = qPartion_l(arr_0, l, r); //change this function according to pivot position
-
+        int p = qPartion_l(arr_0, l, r); 
         quickSort(arr_0, l, p-1);
         quickSort(arr_0, p+1, r);
     }
